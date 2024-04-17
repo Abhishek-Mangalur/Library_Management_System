685777e9ee3ffa4d3decc73ea3256384919902c8
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2024-04-18 03:26:51 +0530

    LMS

---
+++ deletelending.php
@@ -0,0 +1,15 @@
+<?php
+    include "connection.php";
+    error_reporting(0);
+
+    $title = $_GET['rn'];
+    $query = "DELETE FROM lending WHERE title = '$title'";
+
+    $data = mysqli_query($conn,$query);
+    if($data){
+        echo "<script>window.open('adminhome.php','_self')</script>";
+    }else{
+        echo "Failed";
+    }
+    header('Location: adminhome.php');
+?>
