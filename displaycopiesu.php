4eecbb003118c534335d6dbe85b8476b107341b5
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2024-05-14 17:45:43 +0530

    LMS

---
+++ displaycopiesu.php
@@ -0,0 +1,46 @@
+<?php
+    include "connection.php";
+    $query = "select * from copies";
+    $data = mysqli_query($conn,$query);
+    $total = mysqli_num_rows($data);
+    $result = $data;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Copies</title>
+    <link rel="stylesheet" href="displaystyle.css">
+</head>
+
+<body>
+    <table class="tbl" style="border-collapse: collapse;">
+        <thead>
+            <tr>
+                <th scope="col" class="table1">Sl No</th>
+                <th scope="col" class="table1">Book Title</th>
+                <th scope="col" class="table1">No of Copies</th>
+            </tr>
+
+            <?php 
+            if($result){
+                $i=1;
+                while($result = mysqli_fetch_assoc($data)){
+                    echo "
+                    <tr>
+                        <td class='c1'>".$i++."</td>
+                        <td class='c1'>".$result['title']."</td>
+                        <td class='c1'>".$result['copi']."</td>
+                    </tr>
+                    ";
+                }
+            }else{
+                echo "No data found";
+            }
+            ?>
+        </thead>
+    </table>
+</body>
+</html>
