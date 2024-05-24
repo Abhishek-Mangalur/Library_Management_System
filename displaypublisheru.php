2214da9bd202bcd5aba9e1da7f1a9b523edf95e2
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2024-05-24 14:58:28 +0530

    LMS

---
+++ displaypublisheru.php
@@ -0,0 +1,48 @@
+<?php
+    include "connection.php";
+    $query = "select * from publisher";
+    $data = mysqli_query($conn,$query);
+    $total = mysqli_num_rows($data);
+    $result = $data;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Publisher</title>
+    <link rel="stylesheet" href="displaystyle.css">
+</head>
+
+<body>
+    <table class="tbl" style="border-collapse: collapse;">
+        <thead>
+            <tr>
+                <th scope="col" class="table1">Sl No</th>
+                <th scope="col" class="table1">Publisher Name</th>
+                <th scope="col" class="table1">Phone No.</th>
+                <th scope="col" class="table1">Address</th>
+            </tr>
+
+            <?php 
+            if($result){
+                $i=1;
+                while($result = mysqli_fetch_assoc($data)){
+                    echo "
+                    <tr>
+                        <td class='c1'>".$i++."</td>
+                        <td class='c1'>".$result['pname']."</td>
+                        <td class='c1'>".$result['phone']."</td>
+                        <td class='c1'>".$result['addr']."</td>
+                    </tr>
+                    ";
+                }
+            }else{
+                echo "No data found";
+            }
+            ?>
+        </thead>
+    </table>
+</body>
+</html>
