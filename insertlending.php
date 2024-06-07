2f9e2896559c6b02d485e2487bac6ed1201f68e4
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2024-06-07 08:01:33 +0530

    LMS

---
+++ insertlending.php
@@ -0,0 +1,93 @@
+<?php
+    include "connection.php";
+    // session_start();
+    // if(isset($_SESSION['uname']) && isset($_SESSION['pass']))
+    // {
+?>
+
+<?php
+    include "connection.php";
+    if(isset($_POST['submit'])){
+        $dout=$_POST['dout'];
+        $ddate=$_POST['ddate'];
+        $aname=$_POST['aname'];
+        $title=$_POST['title'];
+
+        $sql="INSERT INTO lending(dout, ddate, aname, title) VALUES('$dout', '$ddate', '$aname', '$title')";
+        if(mysqli_query($conn, $sql)){
+            echo "<script>alert('New Lending Added..!')</script>";
+            echo "<script>window.open('adminhome.php','_self')</script>";
+        }else{
+            echo "error".mysqli_error($conn);
+        }
+        mysqli_close($conn);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Publisher</title>
+    <link rel="stylesheet" href="insertstyle.css">
+</head>
+
+<body>
+    <form action="insertlending.php" method="POST">
+        <div class="div1">
+            <label for="Name">Date Out</label><br>
+            <input type="date" name="dout" required><br>
+            <label for="Name">Due Date</label><br>
+            <input type="date" name="ddate" required><br>
+            <label for="Name">Author Name</label><br>
+            <select name="aname" id="opt">  
+                <option>Select Options</option>
+                <?php
+                    $sql = "SELECT * FROM `author`";
+                    $result = mysqli_query($conn ,$sql) or die("Query Failed");
+                    while($row = mysqli_fetch_assoc($result)){
+                ?>
+
+                    <option value="<?php echo  $row['aname']; ?>" >
+                        <?php 
+                            echo $row['aname'];
+                        ?>
+                    </option>
+
+                <?php 
+                    } 
+                ?>
+            </select>
+            <label for="Name">Book Title</label><br>
+            <select name="title" id="opt">  
+                <option>Select Options</option>
+                <?php
+                    $sql = "SELECT * FROM `book`";
+                    $result = mysqli_query($conn ,$sql) or die("Query Failed");
+                    while($row = mysqli_fetch_assoc($result)){
+                ?>
+
+                    <option value="<?php echo  $row['title']; ?>" >
+                        <?php 
+                            echo $row['title'];
+                        ?>
+                    </option>
+
+                <?php 
+                    } 
+                ?>
+            </select>
+
+            <button type="submit" name="submit" value="submit" id="i1">Submit</button>
+        </div>
+    </form>
+</body>
+</html>
+
+<?php
+// } else{
+//     header("Location: login.php");
+//     exit();
+// }
+?>
